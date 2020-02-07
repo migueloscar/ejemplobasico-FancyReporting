bc227102570345bebbd6f8c3a0c8534ac9de9358
Miguel Aguirre <migueloscarap@gmail.com>
2020-02-07 04:38:50 -0500

    agregando proyecto

---
+++ ProyectoPruebas/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ProyectoPruebas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_GenReporte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_GenReporte
+            // 
+            this.btn_GenReporte.Location = new System.Drawing.Point(311, 45);
+            this.btn_GenReporte.Name = "btn_GenReporte";
+            this.btn_GenReporte.Size = new System.Drawing.Size(153, 42);
+            this.btn_GenReporte.TabIndex = 0;
+            this.btn_GenReporte.Text = "Generar Reporte";
+            this.btn_GenReporte.UseVisualStyleBackColor = true;
+            this.btn_GenReporte.Click += new System.EventHandler(this.btn_GenReporte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 127);
+            this.Controls.Add(this.btn_GenReporte);
+            this.Name = "Form1";
+            this.Text = "EjemploImplementacion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_GenReporte;
+    }
+}
+
---
+++ ProyectoPruebas/Form1.cs
@@ -0,0 +1,82 @@
+﻿using FancyReporting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace ProyectoPruebas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_GenReporte_Click(object sender, EventArgs e)
+        {
+            //obteniendo datos
+            string datosReporte = JsonConvert.SerializeObject(DatosIndicadores.getDatos());
+            string configuracionJson = System.IO.File.ReadAllText(@"C:\modulosGeneradorInforme\PruebaBasica\configuracion.json");
+
+            //generando reporte
+            GeneracionInforme informeParte = new GeneracionInforme();
+            MemoryStream file = new MemoryStream();
+            file = informeParte.generarReporte(TipoReporte.InformeIVPeriodos, datosReporte, configuracionJson);
+
+            //guardando el archivo de word
+            file.Seek(0, SeekOrigin.Begin);
+            SaveFileDialog SaveFileDialog = new SaveFileDialog();
+            SaveFileDialog.OverwritePrompt = true;
+            SaveFileDialog.FileName = "ReporteEjemplo";
+            SaveFileDialog.DefaultExt = "docx";
+            SaveFileDialog.Filter ="Word documents (*.docx)|*.docx|All files (*.*)|*.*";
+            SaveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            DialogResult result = SaveFileDialog.ShowDialog();
+            Stream fileStream;
+            if (result == DialogResult.OK)
+            {
+                fileStream = SaveFileDialog.OpenFile();
+
+                file.WriteTo(fileStream);
+                fileStream.Close();
+            }
+        }
+
+    }
+    public class DatosIndicadores
+    {
+        public string detalle { get; set; }
+        public DateTime periodo { get; set; }
+        public double valor { get; set; }
+
+        public static List<DatosIndicadores> getDatos()
+        {
+            List<DatosIndicadores> listDatos =new List<DatosIndicadores>();
+
+            listDatos.Add(new DatosIndicadores() { detalle = "Caja", periodo = new DateTime(2017, 12, 31), valor = 3500 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Caja", periodo = new DateTime(2018, 2, 28), valor = 2500 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Caja", periodo = new DateTime(2018, 12, 31), valor = 3000 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Caja", periodo = new DateTime(2019, 2, 28), valor = 4000 });
+
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Cobrar", periodo = new DateTime(2017, 12, 31), valor = 5000 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Cobrar", periodo = new DateTime(2018, 2, 28), valor = 5400 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Cobrar", periodo = new DateTime(2018, 12, 31), valor = 5300 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Cobrar", periodo = new DateTime(2019, 2, 28), valor = 6000 });
+
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Pagar", periodo = new DateTime(2017, 12, 31), valor = 6000 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Pagar", periodo = new DateTime(2018, 2, 28), valor = 5500 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Pagar", periodo = new DateTime(2018, 12, 31), valor = 5000 }); 
+            listDatos.Add(new DatosIndicadores() { detalle = "Cuentas por Pagar", periodo = new DateTime(2019, 2, 28), valor = 7000 });
+
+            return listDatos;
+        }
+    }
+}
